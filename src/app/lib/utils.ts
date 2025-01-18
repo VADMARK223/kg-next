335dfbd67be6def0608f2449cc9b60698a0dfff6
v.markitanov <v.markitanov@outlook.com>
2025-01-18 07:32:58 +0300

    1.13.333 Version.

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -4,5 +4,5 @@
  * @author Markitanov Vadim
  * @since 10.01.2025
  */
-export const APP_VERSION = '1.13.322'
+export const APP_VERSION = '1.13.333'
 export const isDevMode = (): boolean => process.env.NODE_ENV === 'development'
